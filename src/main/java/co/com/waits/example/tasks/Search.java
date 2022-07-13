fe685bce211728f94cb1fb1f34b56ba2690aefd7
JohnRendon <john.rendon@globant.com>
2022-07-13 15:54:53 -0500

    Change loader validation to implicit wait

--- src/main/java/co/com/waits/example/tasks/Search.java
+++ src/main/java/co/com/waits/example/tasks/Search.java
@@ -1,24 +1,19 @@
 package co.com.waits.example.tasks;
 
-import co.com.waits.example.exceptions.LoaderStillPresent;
-import co.com.waits.example.interactions.Pause;
+import co.com.waits.example.interactions.WaitLoading;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.Tasks;
-import net.serenitybdd.screenplay.actions.Click;
-import net.serenitybdd.screenplay.actions.Enter;
+import net.serenitybdd.screenplay.actions.*;
 import net.serenitybdd.screenplay.conditions.Check;
 import net.serenitybdd.screenplay.waits.WaitUntil;
 
 import java.util.List;
 
-import static co.com.waits.example.userinterfaces.LoaderPage.LOADER;
 import static co.com.waits.example.userinterfaces.SearchFlightPage.*;
 import static co.com.waits.example.userinterfaces.SelectTicketPage.CLOSE_COOKIES;
-import static net.serenitybdd.screenplay.EventualConsequence.eventually;
-import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
 import static net.serenitybdd.screenplay.matchers.WebElementStateMatchers.*;
-import static net.serenitybdd.screenplay.questions.WebElementQuestion.the;
+
 
 
 public class Search implements Task {
@@ -51,7 +46,6 @@ public class Search implements Task {
                 WaitUntil.the(SELECT_CITY.of(flightData.get(0)), isClickable()),
                 Click.on(SELECT_CITY.of(flightData.get(0))));
 
-        //actor.attemptsTo(Pause.forTime(300));
         actor.attemptsTo(WaitUntil.the(DESTINATION, isClickable()));
         actor.attemptsTo(
                 Click.on(DESTINATION),
@@ -61,36 +55,23 @@ public class Search implements Task {
                 WaitUntil.the(SELECT_CITY.of(flightData.get(1)), isClickable()),
                 Click.on(SELECT_CITY.of(flightData.get(1))));
 
-        //actor.attemptsTo(Pause.forTime(300));
         actor.attemptsTo(
                 WaitUntil.the(PASSENGERS, isVisible()),
                 Click.on(PASSENGERS),
                 Click.on(ADD_PASSENGER),
                 Click.on(CLOSE_PASSENGERS));
 
-        //actor.attemptsTo(Pause.forTime(300));
-
         actor.attemptsTo(
                 WaitUntil.the(START_DATE, isCurrentlyVisible()),
                 Click.on(START_DATE),
                 SelectDates.inDatePicker(flightData.get(2), flightData.get(3)));
 
-        //actor.attemptsTo(Pause.forTime(300));
-
         actor.attemptsTo(
                 WaitUntil.the(SEARCH_FLIGHTS, isCurrentlyVisible()),
                 Click.on(SEARCH_FLIGHTS));
 
-        //actor.attemptsTo(WaitUntil.the(LOADER, isNotCurrentlyVisible()).forNoMoreThan(30).seconds());
-        actor.should(eventually(seeThat(the(LOADER), isNotCurrentlyVisible()))
-                .waitingForNoLongerThan(waitTime).seconds()
-                .orComplainWith(LoaderStillPresent.class, String.format("The loader still is present after %s seconds", waitTime))
-        );
+        actor.attemptsTo(WaitLoading.modal());
 
-        actor.attemptsTo(
-                Check.whether(MISSING_DATA.resolveFor(actor).isVisible())
-                        .andIfSo(Click.on(ACCEPT_MISSING_DATA),
-                                Click.on(SEARCH_FLIGHTS)));
 
     }
 }
